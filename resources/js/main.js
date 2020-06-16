4ce0354fe2ec89f15b3be3f465a39d425faefaac
Andrew Oplas <andrewmoplas@gmail.com>
2020-06-16 23:05:01 +0800

    Resolve issues

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,27 +1,18 @@
-const initializeMobileNavMenu = (element) => {
-    if ($(element).length) {
-        const mm = new MetisMenu(element).on("shown.metisMenu", function (event) {
-            window.addEventListener("click", function mmClick(e) {
-                if (!event.target.contains(e.target)) {
-                    mm.hide(event.detail.shownElement);
-                    window.removeEventListener("click", mmClick);
-                }
-            });
-        });
-    }
-};
-
 function initializePage() {
-    initializeMobileNavMenu(".mobile-nav #menu");
-    initializeMobileNavMenu(".page-header-mobile #page-header-menu");
-
-    $(".nav-item-dropdown").each(function () {
-        const mainList = $(this).find(".main-list");
-
-        if (mainList.length) {
-            const id = mainList.attr("id");
-            initializeMobileNavMenu(".nav-item-dropdown #" + id);
+    $(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
+        if (!$(this).next().hasClass("show")) {
+            $(this).parents(".dropdown-menu").first().find(".show").removeClass("show");
         }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass("show");
+
+        $(this)
+            .parents("li.nav-item.dropdown.show")
+            .on("hidden.bs.dropdown", function (e) {
+                $(".dropdown-submenu .show").removeClass("show");
+            });
+
+        return false;
     });
 
     $(window).scroll(function () {
